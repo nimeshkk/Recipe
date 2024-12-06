5d8cbe122fbd09687d771297348c876aab43d1a7
nimeshkk <nimeshkliyanage@gmail.com>
2024-12-06 19:48:57 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
+
 const mongoose = require('mongoose');
+require('dotenv').config(); 
 
 const connectDB = async () => {
    try {
-      const conn = await mongoose.connect(
-        'mongodb+srv://slhit2021:EqLTOtvBfTUzSM11@cluster0.oyd0q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-     );
+      const conn = await mongoose.connect(process.env.MONGO_URI);
       console.log(`MongoDB Connected`);
    } catch (error) {
       console.error(`Error: ${error.message}`);
